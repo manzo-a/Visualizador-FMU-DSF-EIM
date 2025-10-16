84123b0816353ac8ffaa8459e4feacf47b9e2db0
amanzo <manzo.coinary@gmail.com>
2025-10-16 17:16:38 -0300

    Fix test_compile

--- test_compile.py
+++ test_compile.py
@@ -1,6 +1,7 @@
 # test_compile.py
 import os
-from fmpy.util import compile_platform_binary
+import fmpy
+#from fmpy.util import compile_platform_binary
 
 FMU_FILENAME = 'SpringDamperSystem_ME.fmu'
 
